4e5716919e4183792b8e2063ba5633da42d7c984
Veronica Alexandra <veronica.micanquer@upb.edu.co>
2026-01-23 15:04:02 -0500

    Añade script de numeros primos en Python

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,2 +1,19 @@
 # mi_primer_script.py
 print("¡Hola, GitHub!")
+
+def es_primo(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+primos = []
+
+for numero in range(1, 1001):
+    if es_primo(numero):
+        primos.append(numero)
+
+print("Números primos entre 1 y 1000:")
+print(primos)
